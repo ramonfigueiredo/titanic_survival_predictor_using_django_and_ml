a6b7de68697eaf2f4ac7bdfa2842d8cf57015757
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2023-10-18 18:02:36 -0700

    Create REST API.
    - Using Django REST Framework
    - CRUD users
    - CRUD groups
    - Create UserSerializer and GroupSerializer
    - url / -> show the api
    - url /predict -> show form to make predictions
    - url /predict/result/ -> show the prediction

--- titanic/base/urls.py
+++ titanic/base/urls.py
@@ -1,9 +1,17 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
-from .views import home, result
+from .views import home, result, UserViewSet, GroupViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('result/', result, name='result')
+    path('predict/', home, name='home'),
+    path('predict/result/', result, name='result'),
+    # Wire up the API using automatic URL routing.
+    # Additionally, include login URLs for the browsable API.
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- titanic/base/views.py
+++ titanic/base/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
 import pickle
+
+from django.contrib.auth.models import User, Group
+from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework import viewsets
+
+from .serializers import UserSerializer, GroupSerializer
+
 
 def home(request):
     return render(request, 'index.html')
@@ -34,3 +41,21 @@ def result(request):
                             parch, fare, embC, embQ, embS)
 
     return render(request, 'result.html', {'result': result})
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
